aefd85ab8dbeec1810ad45029baa965186440b48
luo <3159868825@qq.com>
2022-11-21 18:03:40 +0800

    third commit

--- ssmbuild/src/main/java/com/luo/hh/gitTest.java
+++ ssmbuild/src/main/java/com/luo/hh/gitTest.java
@@ -3,5 +3,6 @@ package com.luo.hh;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
